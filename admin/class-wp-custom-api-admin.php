56efead3dd8e76135f980aed35e85db34c223197
RiFi2k <reilly@reillylowery.io>
2017-07-25 05:40:02 -0400

    Comment out admin enqueues until we need them

--- admin/class-wp-custom-api-admin.php
+++ admin/class-wp-custom-api-admin.php
@@ -61,19 +61,7 @@ class Wp_Custom_Api_Admin {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp_Custom_Api_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp_Custom_Api_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-custom-api-admin.css', array(), $this->version, 'all' );
+		//wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-custom-api-admin.css', array(), $this->version, 'all' );
 
 	}
 
@@ -84,19 +72,7 @@ class Wp_Custom_Api_Admin {
 	 */
 	public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp_Custom_Api_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp_Custom_Api_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-custom-api-admin.js', array( 'jquery' ), $this->version, false );
+		//wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-custom-api-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
